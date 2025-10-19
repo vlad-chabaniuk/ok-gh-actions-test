444a94c76a51d4f7731ea45389358149adfc7b64
Vlad Chabaniuk <vladyslav.chabaniuk@devlight.io>
2025-10-19 14:50:45 +0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+// https://docs.expo.dev/guides/using-eslint/
+module.exports = {
+  extends: ['expo', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+  ignorePatterns: [
+    '/dist/*',
+    '/node_modules/',
+    '/ios/',
+    '/android/',
+    '/storybook-static/',
+    '/.storybook/',
+  ],
+};
