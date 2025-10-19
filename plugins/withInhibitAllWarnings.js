8495bbb54856f10eb65b7c885f30e57893dc70f3
Vlad Chabaniuk <vladyslav.chabaniuk@devlight.io>
2025-10-19 18:13:55 +0300

    last time i even try this

--- plugins/withInhibitAllWarnings.js
+++ plugins/withInhibitAllWarnings.js
@@ -1,10 +1,23 @@
   // plugins/withInhibitAllWarnings.js
 const { withPodfile } = require('@expo/config-plugins');
+const fs = require('fs');
+const path = require('path');
 
 const withInhibitAllWarnings = (config) => {
   return withPodfile(config, (podfileConfig) => {
-    const targetName = config.name;
+      // Dynamically find the Xcode project name in the ios directory
+    const iosDir = path.join(podfileConfig.modRequest.projectRoot, 'ios');
+    const files = fs.readdirSync(iosDir);
+    const xcodeprojFile = files.find(file => file.endsWith('.xcodeproj'));
+    
+    if (!xcodeprojFile) {
+      throw new Error('Could not find .xcodeproj file in ios directory');
+    }
+    
+    const targetName = path.basename(xcodeprojFile, '.xcodeproj');
     const targetLine = `target '${targetName}' do`;
+    
+      // Create the replacement content
     const newContent = `inhibit_all_warnings!\n\n${targetLine}`;
     
     if (podfileConfig.modResults.contents.includes(targetLine)) {
